f75ccd69f2990fcff94b48fc72b4ba53a88bb20c
usuario <usuario@DESKTOP-2FH21K9>
2024-04-05 21:12:59 -0300

    Commit del punto 1 completo

--- tp1_ValleAlejandro/src/ar/edu/unju/fi/ejercicio1/Main.java
+++ tp1_ValleAlejandro/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -3,7 +3,15 @@ package ar.edu.unju.fi.ejercicio1;
 public class Main {
 
 	public static void main(String[] args) {
-		
+		int a, b, c, d, e;
+		float suma, resultado;
+		a = 8;
+		b = 5;
+		c = 1;
+		d = 20;
+		e = 9;
+		suma= a+b+c+d+e;
+		resultado = suma/5;
+		System.out.println("El promedio es: "+ resultado);
 	}
-
 }
